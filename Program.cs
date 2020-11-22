b6dc4b94c77019e61ffffd12c267de367ab8c549
hdebec-dev <74872517+hdebec-dev@users.noreply.github.com>
2020-11-22 18:51:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var nb = rnd.Next(100) + 1;
+
+            while (true)
+            {
+                Console.WriteLine("saissisez un nombre entre 1 et 100");
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                if (value > nb)
+                {
+                    Console.WriteLine("plus grand");
+                }
+                else if (value < nb)
+                {
+                    Console.WriteLine("plus petit");
+                }
+                //il faut gérer le cas ou le nombre est égale au nombre aléatoire générer
+                else
+                {
+                    Console.WriteLine("égalité");
+                    Console.ReadLine();
+                    // je met un break pour quitter la boucle parce que j'ai trouver le même nombre
+                    break;
+                }
+            }
+        }
+    }
+}
